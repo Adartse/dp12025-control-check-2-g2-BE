09c552e0027bb664a4fafce8a41fab62b7af33ae
Adartse <bediliaestrada@gmail.com>
2025-11-27 14:16:07 +0100

    Test 1 - Ok

--- src/main/java/es/us/dp1/chess/federation/regulation/Referee.java
+++ src/main/java/es/us/dp1/chess/federation/regulation/Referee.java
@@ -3,17 +3,27 @@ package es.us.dp1.chess.federation.regulation;
 import java.time.LocalDate;
 import java.util.List;
 
+import es.us.dp1.chess.federation.model.NamedEntity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
-public class Referee{
+@Entity
+public class Referee extends NamedEntity{
 
+    @NotBlank
+    @NotNull
+    @Size(min = 10, max = 10)
     String licenseNumber;
 
+    @NotNull
     LocalDate certificationDate;
     
     @Transient
--- src/main/java/es/us/dp1/chess/federation/regulation/Sanction.java
+++ src/main/java/es/us/dp1/chess/federation/regulation/Sanction.java
@@ -1,19 +1,34 @@
 package es.us.dp1.chess.federation.regulation;
 
 
+import es.us.dp1.chess.federation.model.BaseEntity;
 import es.us.dp1.chess.federation.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Sanction{
+@Entity
+public class Sanction extends BaseEntity{
 
+    @NotBlank
+    @NotNull
+    @Size(min = 15, max = 70)
     String description;
 
+    @DecimalMin("0.01")
     Double monetaryFine;
 
+    @NotNull
+    @Enumerated(EnumType.STRING)
     SanctionType type;
 
     @Transient
