73af9f3f1dda86fadfffb304edb4495e6cf5e84b
Adartse <bediliaestrada@gmail.com>
2025-12-04 14:10:50 +0100

    test1 ok

--- src/main/java/es/us/dp1/chess/federation/regulation/Referee.java
+++ src/main/java/es/us/dp1/chess/federation/regulation/Referee.java
@@ -2,18 +2,30 @@ package es.us.dp1.chess.federation.regulation;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.jar.Attributes.Name;
 
+import es.us.dp1.chess.federation.model.NamedEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
-public class Referee{
+@Entity
+public class Referee extends NamedEntity {
 
+    @Size(min = 10, max = 10)
+    @NotNull
+    @NotBlank
     String licenseNumber;
 
+    @NotNull
     LocalDate certificationDate;
     
     @Transient
--- src/main/java/es/us/dp1/chess/federation/regulation/Sanction.java
+++ src/main/java/es/us/dp1/chess/federation/regulation/Sanction.java
@@ -1,19 +1,36 @@
 package es.us.dp1.chess.federation.regulation;
 
 
+import es.us.dp1.chess.federation.model.BaseEntity;
 import es.us.dp1.chess.federation.user.User;
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Sanction{
+@Entity
+public class Sanction extends BaseEntity{
 
+    @Size(min = 15, max = 70)
+    @NonNull
+    @NotBlank
     String description;
 
+    @DecimalMin("0.01")
     Double monetaryFine;
 
+    @Enumerated(EnumType.STRING)
+    @NotNull
     SanctionType type;
 
     @Transient
